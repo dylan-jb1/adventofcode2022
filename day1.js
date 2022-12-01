9204787b287fd11975deec7451bb711fefd09b42
dylan-jb1 <89441511+dylan-jb1@users.noreply.github.com>
2022-12-01 15:12:06 +1000

    Day1

---
+++ day1.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const readline = require('readline')
+
+async function read() {
+    const fileStream = fs.createReadStream(__dirname+"/day1_input.txt");
+    
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let first = 0;
+    let second = 0;
+    let third = 0;
+    let total = 0;
+    for await (const line of rl) {
+        if (line.trim() == "") {
+            if (total>first) {
+                third=second;
+                second=first;
+                first=total;
+            } else if (total > second) {
+                third=second;
+                second=total;
+            } else if (total > third) {
+                third=total;
+            }
+            total = 0;
+        } else {
+            total+=parseInt(line);
+        }
+    }   
+    console.log(first+second+third);
+}
+
+read();
